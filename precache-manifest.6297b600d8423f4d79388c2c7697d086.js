a94dd665e432198600e7a37e6cbfd71263c7a00a
DenmarFel <denmarfeliciano@gmail.com>
2020-03-27 23:05:41 -0700

    Updates

---
+++ precache-manifest.6297b600d8423f4d79388c2c7697d086.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bbc756fc3f3dabfec34546156f0bf24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5718b120ece24a5ebb41",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "44e05399626d10736e98",
+    "url": "/static/js/2.56c31db7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c31db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5718b120ece24a5ebb41",
+    "url": "/static/js/main.910146f7.chunk.js"
+  },
+  {
+    "revision": "c21fe37de23a3f68f6e0",
+    "url": "/static/js/runtime-main.a68b0f98.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
